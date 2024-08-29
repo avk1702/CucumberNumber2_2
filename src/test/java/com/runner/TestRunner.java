53120fa3c633d7cf549bd98f8e35759fb28f8f0a
avk1702 <avk1702@bk.ru>
2024-08-29 23:08:14 +0500

    Double3

--- src/test/java/com/runner/TestRunner.java
+++ src/test/java/com/runner/TestRunner.java
@@ -11,7 +11,7 @@ import static io.cucumber.core.options.Constants.*;
         @ConfigurationParameter(key = FILTER_TAGS_PROPERTY_NAME, value = "@all"),
         @ConfigurationParameter(key = FEATURES_PROPERTY_NAME, value = "src/test/resources/features"),
         @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "com.steps"),
-        @ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "io.gameta.allure.cucumber7jvm.AllureCucumber7Jvm, pretty")
+        @ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm, pretty")
 })
 
 public class TestRunner {

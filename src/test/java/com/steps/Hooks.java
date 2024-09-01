10423cc77c748a84e9de876194718991f441b676
avk1702 <avk1702@bk.ru>
2024-09-01 15:53:35 +0500

    Jenkins5

--- src/test/java/com/steps/Hooks.java
+++ src/test/java/com/steps/Hooks.java
@@ -12,6 +12,9 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.time.Duration;
+import java.util.Map;
+
+
 public class Hooks {
 
     private static WebDriver driver;
@@ -26,27 +29,16 @@ public class Hooks {
     }
 
     @Before
-    public void setUp() {
-        String runMode = System.getProperty("run.mode", "local");
-        if ("remote".equalsIgnoreCase(runMode)) {
-            // Setup for remote execution with Selenoid
-            try {
-                URL remoteUrl = new URL("http://149.154.71.152:4444/wd/hub");
-                DesiredCapabilities capabilities = new DesiredCapabilities();
-                capabilities.setBrowserName("chrome");
-                capabilities.setVersion("109");
-                capabilities.setCapability("enableVnc", true);
-                driver = new RemoteWebDriver(remoteUrl, capabilities);
-            } catch (MalformedURLException e) {
-                throw new RuntimeException("Invalid remote URL", e);
-            }
-        } else {
-            // Local execution
-            System.setProperty("webdriver.chrome.driver", "src\\test\\resources\\chromedriver.exe");
-            ChromeOptions options = new ChromeOptions();
-            options.addArguments("start-maximized");
-            driver = new ChromeDriver(options);
-        }
+    public void setUp() throws MalformedURLException{
+        URL remoteUrl = new URL("http://149.154.71.152:4444/wd/hub");
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setBrowserName("chrome");
+        capabilities.setVersion("109");
+        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+                        "enableVNC" , true,
+                        "enableVideo", false));
+
+        driver = new RemoteWebDriver(remoteUrl, capabilities);
         wait = new WebDriverWait(driver, Duration.ofSeconds(20));
     }
 
